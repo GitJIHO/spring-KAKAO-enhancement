4245b9183477388ebd340b8cd3d8db1c8d10b4fa
gitjiho <jiho99322@gmail.com>
2024-07-15 15:34:07 +0900

    refactor: 사용하지 않는 setter 삭제 및 기본생성자 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -28,16 +28,8 @@ public class ProductRequest {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPrice() {
         return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public String getImg() {
